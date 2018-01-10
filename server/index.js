6c4c57b2cbb09ae39b3cfd333578174446841754
andino <andy.andino93@gmail.com>
2018-01-09 22:55:35 -0800

    init basic server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import {} from 'dotenv/config';
+
+const app = express();
+const port = process.env.PORT;
+
+app.listen(port, () => console.log(`express is listening on ${port}`));
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('hello there');
+});
