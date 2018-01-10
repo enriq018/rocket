7aa5661d0211dd99f327291b692923b70c2cb943
andino <andy.andino93@gmail.com>
2018-01-09 22:57:18 -0800

    forgot semicolon

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -2,13 +2,13 @@ import { expect, assert } from 'chai';
 import axios from 'axios';
 import {} from 'dotenv/config';
 import 'babel-polyfill'; // for async/await compiling
-import '../server'
+import '../server';
 
 describe('Express Server', () => {
   after(() => {
     process.exit(2)
   });
-  
+
   it('should respond to GET with 200 status', async () => {
     const response = await axios.get(`http://127.0.0.1:${process.env.PORT}`);
     expect(response.status).to.equal(200);
