adfd60741bb89dbf082def412f363ba7b31f9fef
andino <andy.andino93@gmail.com>
2018-01-24 17:16:05 -0800

    add webpack middleware and rename to js extension

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
+import {} from 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import {} from 'dotenv/config';
 
 const app = express();
 const port = process.env.PORT || 1337;
 
 app.listen(port, () => console.log(`express is listening on ${port}`)); // eslint-disable-line
+
+// disable for global requires since airbnb doesn't like them being in a block
+if (process.env.NODE === 'development') {
+  /* eslint-disable */
+  const config = require('../webpack.dev.js');
+  const compiler = require('webpack')(config);
+  app.use(require('webpackDevMiddleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath,
+  }));
+  app.use(require('webpackHotMiddleware')(compiler));
+  /* eslint-enable */
+}
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
